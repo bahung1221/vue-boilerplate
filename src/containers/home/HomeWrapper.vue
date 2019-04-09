2798f7c8d05eb5dd8f5089b5ef27dbb7785c7fb8
bahung <bahung1221@gmail.com>
2019-04-09 19:41:47 +0700

    Fix router

---
+++ src/containers/home/HomeWrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" v-cloak>
+  <custom-header/>
+    <custom-sidebar/>
+    <div>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/common/Header.vue'
+import Sidebar from '@/components/common/Sidebar.vue'
+
+export default {
+  components: {
+    CustomHeader: Header,
+    CustomSidebar: Sidebar,
+  },
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -7,9 +7,19 @@ export default new Router({
   mode: 'history',
   routes: [
     {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/containers/login/Index.vue'),
+    },
+    {
       path: '/',
-      name: 'Home',
-      component: () => import('@/containers/home/Index.vue'),
+      component: () => import('@/containers/home/HomeWrapper.vue'),
+      children: [
+        {
+          path: '',
+          component: () => import('@/containers/home/Index.vue'),
+        },
+      ],
     },
   ],
 })
