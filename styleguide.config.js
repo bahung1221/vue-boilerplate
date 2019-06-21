bad6a17a5dcab12e4a48a5e79b92353a7b91b6e3
Nguyễn Bá Hưng <bahung1221@gmail.com>
2019-06-21 11:38:32 +0700

    Update depedencies

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,11 @@
+const utils = require('./build/utils')
+const merge = require('webpack-merge')
+
 module.exports = {
-  webpackConfig: require('./build/webpack.prod.conf'),
+  webpackConfig: merge(require('./build/webpack.base.conf'), {
+    module: {
+      rules: utils.styleLoaders(),
+    },
+  }),
+  components: 'src/components/**/[A-Z]*.vue',
 }
---
+++ styleguide.prod.config.js
@@ -0,0 +1,6 @@
+// const utils = require('./build/utils')
+// const merge = require('webpack-merge')
+
+module.exports = {
+  webpackConfig: require('./build/webpack.prod.conf'),
+}
